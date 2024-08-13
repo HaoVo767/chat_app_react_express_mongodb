b41c45e4a62629816be0983190188516d621af24
minh.hao <minhhao767@gmail.com>
2024-08-14 01:25:02 +0700

    update

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: "https://chat-app-react-express-mongodb.vercel.app/" }));
 
 connectDB.connect();
 // Router(app);
