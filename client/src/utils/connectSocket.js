8be16c28015813cc6b2ba28a770c8dcebd0f7b2f
minh.hao <minhhao767@gmail.com>
2024-08-18 18:52:14 +0700

    update 10

--- client/src/utils/connectSocket.js
+++ client/src/utils/connectSocket.js
@@ -1,3 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://server-kappa-self.vercel.app/");
+export const socket = io("https://server-kappa-self.vercel.app/", {
+  withCredentials: true,
+});
