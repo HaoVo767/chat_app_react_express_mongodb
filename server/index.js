83cf15be34c79e37cac92766c0b0dd915349a406
minh.hao <minhhao767@gmail.com>
2024-08-22 13:14:12 +0700

    update

--- server/index.js
+++ server/index.js
@@ -4,24 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./database");
 const routers = require("./Routes");
 
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-
 const app = express();
-
-const server = createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-});
-io.listen(8080, (err) => {
-  if (err) {
-    console.log("error ", err);
-  } else {
-    console.log("connec socket success");
-  }
-});
 
 const corsOptions = {
   origin: "*",
@@ -43,56 +26,11 @@ app.get("/", (req, res) => {
   res.status(200).send({ author: "Hao" });
 });
 
-const onlineUsers = [];
-io.on("connection", (socket) => {
-  console.log("new connection", socket.id);
-
-  socket.on("addNewUser", (userId) => {
-    if (userId) {
-      console.log("user ID ", userId);
-      if (onlineUsers.some((onlineUser) => onlineUser?.userId === userId)) {
-        const exitsUser = onlineUsers.find((onlineUser) => onlineUser.userId === userId);
-        onlineUsers.splice(onlineUsers.indexOf(exitsUser), 1);
-        onlineUsers.push({
-          userId,
-          socketId: socket.id,
-        });
-      } else {
-        onlineUsers.push({
-          userId,
-          socketId: socket.id,
-        });
-      }
-    }
-    io.emit("getUsersOnline", onlineUsers);
-  });
-  console.log("online users", onlineUsers);
-  socket.on("disconnect", () => {
-    const newOnlineUsers = onlineUsers.filter((onlineUser) => onlineUser.socketId !== socket.id);
-    io.emit("getUsersOnline", newOnlineUsers);
-  });
-  socket.on("sendMessage", (message) => {
-    console.log("message send from socket", socket.id);
-    const user = onlineUsers.find((user) => user?.userId === message?.recipientId);
-    console.log(message);
-    if (user) {
-      console.log("this is user", user);
-      io.to(user?.socketId).emit("getMessage", message);
-      io.to(user?.socketId).emit("getNotifications", {
-        ...message,
-        isRead: false,
-        date: new Date(),
-      });
-    }
-  });
-});
-
 const port = process.env.PORT || 3000;
-server.listen(port, function (err) {
+app.listen(port, function (err) {
   if (err) {
     console.log("server error: " + err);
   }
   console.log("app listening on port " + port);
 });
-
-// module.exports = server;
+module.exports = app;
